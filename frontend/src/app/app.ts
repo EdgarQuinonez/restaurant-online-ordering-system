6495d9007c87142ca5dbbc44703c6ffa75b115b1
EdgarQuinonez <l22410043@itparral.edu.mx>
2025-09-05 08:35:15 -0600

    Update @angular/core, test page includes ButtonModule from primeng

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Test } from '@pages/test/test';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Test],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
